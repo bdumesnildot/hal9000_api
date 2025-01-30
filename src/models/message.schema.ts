3df868853ce0a5f0b2f5a22c9fce58ea3df02985
bdumesnildot <benoit.du-mesnildot@jdc.fr>
2025-01-30 10:34:30 +0100

    🧹 cleaning

--- src/models/message.schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod"
-
-export const coreMessageSchema = z.object({
-  role: z.enum(["system", "user", "data", "assistant"]),
-  content: z.string(),
-})
-
-export const coreMessageListSchema = coreMessageSchema.array()
