97e67b5c5bd675b2b25e0fde086507e4350a1fc4
bdumesnildot <benoit.du-mesnildot@jdc.fr>
2025-01-30 10:35:18 +0100

    add deno_env and ollama_url env variables

--- src/env.ts
+++ src/env.ts
@@ -1,22 +1,17 @@
-import { createEnv } from '@t3-oss/env-core'
-import { z } from "zod";
+import { createEnv } from "@t3-oss/env-core"
+import { z } from "zod"
 
 export const env = createEnv({
-  /*
-   * Specify what prefix the client-side variables must have.
-   * This is enforced both on type-level and at runtime.
-   */
-  skipValidation: Deno.env.get("SKIP_ENV_VALIDATION") === 'true',
+
+  skipValidation: Deno.env.get("SKIP_ENV_VALIDATION") === "true",
   server: {
-      PORT: z.number().default(8888),
-      SERVICE_KEY: z.string(),
-      OPENAI_API_KEY: z.string(),
-      GOOGLE_API_KEY: z.string(),
-      ANTHROPIC_API_KEY: z.string(),
+    DENO_ENV: z.enum(["development", "production"]).default("development"),
+    PORT: z.number().default(8888),
+    SERVICE_KEY: z.string(),
+    OPENAI_API_KEY: z.string(),
+    GOOGLE_API_KEY: z.string(),
+    ANTHROPIC_API_KEY: z.string(),
+    OLLAMA_BASE_URL: z.string().url(),
   },
-  /**
-   * What object holds the environment variables at runtime.
-   * Often `process.env` or `import.meta.env`
-   */
   runtimeEnv: Deno.env.toObject(),
 })
